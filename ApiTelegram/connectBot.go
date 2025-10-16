7d7c85614d517731acb32e081a059579e063c16c
Cristian Molina <cm1094871@gmail.com>
2025-10-16 17:01:23 -0500

    conexion y creacion nuevo canal Telegram

--- ApiTelegram/connectBot.go
+++ ApiTelegram/connectBot.go
@@ -1,32 +1,37 @@
 package apitelegram
 
 import (
-	"fmt"
+	"database/sql"
+	// "fmt"
 	"log"
+	sendmessagetelegram "modulo/SendMessageTelegram"
+	"modulo/repository"
 	"os"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
 
-func BotTelegram() {
+func BotTelegram(db *sql.DB) {
 	// Datos provenientes del .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error al cargar archivo .env")
 	}
 	// Cadena de conexion con variables de entorno
-	cadenaConexionBot := fmt.Sprintf("token=%v",
-		os.Getenv("TELEGRAM_TOKEN"),
-	)
-
+	a:= os.Getenv("TELEGRAM_TOKEN")
+	
 	// creacion de instancia de bot segun token
-	bot, err := tgbotapi.NewBotAPI(cadenaConexionBot)
+	bot, err := tgbotapi.NewBotAPI(a)
 	if err != nil {
 		log.Println("Error al crear el bot:", err)
 	}
 	// Nombre de usuario del bot
 	log.Printf("Bot autorizado como: %v", bot.Self.UserName)
+
+	// Guarda la instancia del bot para siempre trabajar con el mismo bot 
+	sendmessagetelegram.Init(bot)
 
 	// Estructura para poder recibir mensajes nuevos
 	u := tgbotapi.NewUpdate(0)
@@ -36,11 +41,21 @@ func BotTelegram() {
 
 	// Se procesa cada mensaje recibido
 	for update := range updates {
-		if update.Message != nil {
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
+		if update.Message == nil {
+			continue
 		}
+
+		// Atributos nuevo usuario y chat de telegram
+		chatID := update.Message.Chat.ID
+		username := update.Message.From.UserName
+		telefono := "0000000000"
+		canal := update.Message.Chat.Type
+		fecha := time.Now()
+
+		err := repository.QueryUser(db, chatID, username, telefono, canal, fecha)
+		if err != nil {
+			sendmessagetelegram.MessageUser(chatID, "error registrando usuario en la base de datos")
+		}
+		sendmessagetelegram.MessageUser(chatID, "usuario registrado correctamente")
 	}
 }
