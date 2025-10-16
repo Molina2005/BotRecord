92485f9428e01904a5b7d91f72a56ebe3c5681e0
Cristian Molina <cm1094871@gmail.com>
2025-10-16 17:04:25 -0500

    consultas a base de datos postgres

---
+++ repository/query.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"database/sql"
+	"time"
+)
+
+func QueryUser(db *sql.DB, ChatID int64, name string, phone string, chanel string, date time.Time) error {
+	var existe bool
+	// verificacion de existencia de usuario
+	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM usuarios WHERE Id_usuario=$1)", ChatID).Scan(&existe)
+	if err != nil {
+		return err
+	}
+
+	// Si no existe se crea
+	if !existe {
+		_,err := db.Exec("INSERT INTO usuarios (Id_usuario,nombre,telefono,canal,fecha) VALUES($1,$2,$3,$4,$5)",
+			ChatID, name, phone, chanel, date)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
