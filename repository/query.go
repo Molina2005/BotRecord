103c0b6a08b00a886aefc844259eb7f10d6b61a5
Cristian Molina <cm1094871@gmail.com>
2025-10-18 15:27:06 -0500

    consulta a base de datos postgres

--- repository/query.go
+++ repository/query.go
@@ -15,7 +15,7 @@ func QueryUser(db *sql.DB, userID int64, name string, phone string, chanel strin
 
 	// Si no existe se crea
 	if !existe {
-		_,err := db.Exec("INSERT INTO usuarios (id_usuario,nombre,telefono,canal,fecha) VALUES($1,$2,$3,$4,$5)",
+		_, err := db.Exec("INSERT INTO usuarios (id_usuario,nombre,telefono,canal,fecha) VALUES($1,$2,$3,$4,$5)",
 			userID, name, phone, chanel, date)
 		if err != nil {
 			return err
@@ -24,4 +24,20 @@ func QueryUser(db *sql.DB, userID int64, name string, phone string, chanel strin
 	return nil
 }
 
-// consulta envio recordatorios
+// consulta insertar recordatorios
+func CreateRecord(
+	db *sql.DB, id_user int64,
+	title, description, state, repeat, shipping_chanel, time_record string,
+	date_record time.Time,
+) error {
+
+	_, err := db.Exec(
+		"INSERT INTO recordatorios (id_usuario,titulo,descripcion,fecha_recordatorio,hora_recordatorio,estado,repetir,canal_envio,fecha_creacion) VALUES($1,$2,$3,$4,$5,$6,$7,$8, NOW())",
+		id_user, title, description, date_record, time_record, state, repeat, shipping_chanel)
+	if err != nil {
+		return err
+	}
+
+	
+	return nil
+}
