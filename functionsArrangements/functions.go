99fbb9b69c61287e6cdef2d1c5956231e00721ce
Cristian Molina <cm1094871@gmail.com>
2025-10-20 10:39:46 -0500

    funciones arreglos de datos X

--- functionsArrangements/functions.go
+++ functionsArrangements/functions.go
@@ -3,6 +3,6 @@ package functionsarrangements
 import "time"
 
 func FormatDate(date string) (time.Time, error) {
-	formatDate := "2006-01-02"
+	formatDate := "2006-01-02 15:04"
 	return time.Parse(formatDate, date)
 }
