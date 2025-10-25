e1afd21461fd30ad7be655192998fefd80f8b77e
Cristian Molina <cm1094871@gmail.com>
2025-10-25 14:38:06 -0500

    funciones arreglos datos X

--- functionsArrangements/functions.go
+++ functionsArrangements/functions.go
@@ -1,9 +1,20 @@
 package functionsarrangements
 
-import "time"
+import (
+	"time"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Formateo de fechas y hora
 func FormatDate(date string) (time.Time, error) {
 	formatDate := "2006-01-02 15:04"
 	loc, _ := time.LoadLocation("America/Bogota")
 	return time.ParseInLocation(formatDate, date, loc)
+}
+
+// Cifrado de contraseñas
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
 }
