b48e9a3a7e99958345159413b8eb75ed13b9541d
Cristian Molina <cm1094871@gmail.com>
2025-10-27 13:41:02 -0500

    logica principal

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ import (
 )
 
 func main() {
-	// conexion a la base de datos
+	// Conexion a la base de datos
 	conexion, err := connect.Connect()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// cierre de conexion
+	// Cierre de conexion
 	defer conexion.Close()
 	fmt.Println("Conexion exitosa a PosgreSQL")
 
 	// Conexion y usabilidad bot Telegram
 	apitelegram.BotTelegram(conexion)
-	// tiempo espera
+	// Tiempo espera
 	time.Sleep(30 * time.Minute)
 }
