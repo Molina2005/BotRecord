f5b3f45f6d8bce96a48166a805f3bb8800ddeb6e
Cristian Molina <cm1094871@gmail.com>
2025-11-05 10:05:44 -0500

    logica api bot telegram

--- ApiTelegram/connectBot.go
+++ ApiTelegram/connectBot.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	sendmessagetelegram "modulo/SendMessageTelegram"
+	"modulo/bot"
 	functionsarrangements "modulo/functionsArrangements"
 	"modulo/repository"
 	"os"
@@ -24,7 +25,7 @@ func createUser(db *sql.DB, update *tgbotapi.Update) (int64, string) {
 	partsMessage := strings.SplitN(text, " ", 3)
 
 	if len(partsMessage) < 3 {
-		sendmessagetelegram.MessageUser(chatID, "❗Uso : /registrar NombreUsuario")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Uso : /registrar NombreUsuario", bot.EmojiAtencion))
 		return 0, ""
 	}
 	dateName := partsMessage[1] // Nombre usuario
@@ -33,17 +34,17 @@ func createUser(db *sql.DB, update *tgbotapi.Update) (int64, string) {
 	// Hasheo(encriptacion) contraseña usuario
 	datePassword, err := functionsarrangements.HashPassword(password)
 	if err != nil {
-		sendmessagetelegram.MessageUser(chatID, "⚠️ contraseña incorrecta")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v contraseña incorrecta", bot.EmojiAdvertencia))
 		return 0, ""
 	}
 	date := time.Now()
 
 	err = repository.QueryUser(db, idUser, dateName, date, datePassword)
 	if err != nil {
-		sendmessagetelegram.MessageUser(chatID, "❌ error registrando usuario en la base de datos")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v error registrando usuario en la base de datos", bot.EmojiError))
 		return 0, ""
 	} else {
-		sendmessagetelegram.MessageUser(chatID, "✅ usuario registrado correctamente")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v usuario registrado correctamente", bot.EmojiExito))
 	}
 	return idUser, dateName
 }
@@ -52,7 +53,7 @@ func consultIdUser(db *sql.DB, update *tgbotapi.Update, chatID int64) (int64, er
 	text := update.Message.Text
 	partMessage := strings.SplitN(text, " ", 2)
 	if len(partMessage) < 2 {
-		sendmessagetelegram.MessageUser(chatID, "❗Uso: /consultar contraseña")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Uso: /consultar contraseña", bot.EmojiAtencion))
 		return 0, nil
 	}
 	datePassword := partMessage[1] // Contraseña
@@ -62,7 +63,7 @@ func consultIdUser(db *sql.DB, update *tgbotapi.Update, chatID int64) (int64, er
 		sendmessagetelegram.MessageUser(chatID, err.Error())
 		return 0, err
 	} else {
-		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("🆔 Codigo unico: %v", idUser))
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Codigo unico: %v", bot.EmojiId, idUser))
 	}
 	return idUser, nil
 }
@@ -71,21 +72,21 @@ func deleteUser(db *sql.DB, update *tgbotapi.Update, chatID int64) int64 {
 	text := update.Message.Text
 	partMessage := strings.SplitN(text, " ", 2)
 	if len(partMessage) < 2 {
-		sendmessagetelegram.MessageUser(chatID, "❗Uso: /eliminar IdUsuario")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Uso: /eliminar IdUsuario", bot.EmojiAtencion))
 		return 0
 	}
 	dateId, err := strconv.ParseInt(partMessage[1], 10, 64)
 	if err != nil {
-		sendmessagetelegram.MessageUser(chatID, "⚠️ El ID debe ser un número válido.")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v El ID debe ser un número válido.", bot.EmojiAdvertencia))
 		return 0
 	}
 
 	err = repository.QueryDeleteUser(db, dateId)
 	if err != nil {
-		sendmessagetelegram.MessageUser(chatID, "❌ Error al eliminar usuario de la base de datos")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Error al eliminar usuario de la base de datos", bot.EmojiError))
 		return 0
 	} else {
-		sendmessagetelegram.MessageUser(chatID, "🗑️ Usuario elimiando correctamente")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Usuario elimiando correctamente", bot.EmojiEliminacion))
 	}
 	return dateId
 }
@@ -101,17 +102,17 @@ func createRecord(db *sql.DB, update *tgbotapi.Update, chatID, idUser int64, cha
 	title := parts[3]
 	dateConv, err := functionsarrangements.FormatDate(dateRecord)
 	if err != nil {
-		sendmessagetelegram.MessageUser(chatID, "❗Formato de fecha incorrecto. Usa YYYY-MM-DD HH:MM")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v Formato de fecha incorrecto. Usa YYYY-MM-DD HH:MM", bot.EmojiAtencion))
 		return
 	}
 	state := "pendiente"
 
 	errDB := repository.QueryCreateRecord(db, idUser, title, dateConv, state, channel)
 	if errDB != nil {
-		sendmessagetelegram.MessageUser(chatID, "❌ error registrando recordatorio en la base de datos")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v error registrando recordatorio en la base de datos", bot.EmojiError))
 		return
 	} else {
-		sendmessagetelegram.MessageUser(chatID, "✅ recordatorio registrado correctamente")
+		sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v recordatorio registrado correctamente", bot.EmojiExito))
 	}
 }
 
@@ -129,7 +130,7 @@ func sendReminder(db *sql.DB, chatID int64) {
 			recordDate := r.DateRecord.Truncate(time.Minute)
 			if recordDate.Equal(currentDate) {
 				formatDate := r.DateRecord.Format("15:04")
-				sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("🔔 %v %v", r.Title, formatDate))
+				sendmessagetelegram.MessageUser(chatID, fmt.Sprintf("%v %v %v", bot.EmojiNotificacion, r.Title, formatDate))
 			}
 		}
 		// lapso de 1min para que vuelva repetir el proceso
@@ -141,7 +142,7 @@ func consultReminder(db *sql.DB, update *tgbotapi.Update, ChatID int64) {
 	text := update.Message.Text
 	parts := strings.Split(text, " ")
 	if len(parts) < 1 {
-		sendmessagetelegram.MessageUser(ChatID, "❗Uso: /Lista")
+		sendmessagetelegram.MessageUser(ChatID, fmt.Sprintf("%v Uso: /Lista", bot.EmojiAtencion))
 	}
 	reminders, err := repository.ConsultShippingReminder(db, ChatID)
 	if err != nil {
@@ -150,7 +151,7 @@ func consultReminder(db *sql.DB, update *tgbotapi.Update, ChatID int64) {
 
 	for _, r := range reminders {
 		recordDate := r.DateRecord.Format("2006-01-02 15:04")
-		sendmessagetelegram.MessageUser(ChatID, fmt.Sprintf("⏰ [%v] %v %v %v ", r.IdRecordatorios, r.Title, recordDate, r.Estado))
+		sendmessagetelegram.MessageUser(ChatID, fmt.Sprintf("%v [%v] %v %v %v ", bot.EmojiRecordatorio, r.IdRecordatorios, r.Title, recordDate, r.Estado))
 	}
 }
 
